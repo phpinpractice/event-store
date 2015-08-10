f61344a97ef9c6aec19dbcfe0ecdc6c6d17a8203
Mike van Riel <mike@ingewikkeld.net>
2015-08-10 14:05:57 +0200

    Add test for the Event class and add DocBlocks

--- src/Stream/Event.php
+++ src/Stream/Event.php
@@ -5,6 +5,17 @@ namespace PhpInPractice\EventStore\Stream;
 use PhpInPractice\EventStore\Metadata;
 use PhpInPractice\EventStore\Stream;
 
+/**
+ * Immutable entity representing a single event from the event stream.
+ *
+ * When an instance of this class is created it is not associated with an Event Stream yet and some information may
+ * not be provided yet, such as the sequence number and stream. Using the `withStream` method it is possible to clone
+ * this event and associate it with a stream, which also gives it a sequence number and updates the sequence number
+ * of the stream.
+ *
+ * As a user you generally need not worry about the above; the withStream method is called by the EventStore once the
+ * event is persisted with an active stream.
+ */
 final class Event
 {
     /** @var Event\Id */
@@ -25,6 +36,18 @@ final class Event
     /** @var integer|null */
     private $sequence = null;
 
+    /**
+     * Initialize a new event with an identifier, payload and optionally the date at which it was originally emitted
+     * and some metadata.
+     *
+     * If the date is omitted than the event assumes it is emitted now or within a few moments and thus sets the
+     * emission date to now.
+     *
+     * @param Event\Id                $id
+     * @param Event\Payload           $payload
+     * @param \DateTimeInterface|null $emittedAt
+     * @param Metadata|null           $metadata
+     */
     public function __construct(
         Event\Id $id,
         Event\Payload $payload,
@@ -39,36 +62,89 @@ final class Event
         $this->metadata  = $metadata ?: new Metadata();
     }
 
+    /**
+     * Returns the identifier for this event.
+     * @return Event\Id
+     */
     public function id()
     {
         return $this->id;
     }
 
+    /**
+     * Returns the stream to which this event belongs or null if it isn't emitted yet.
+     *
+     * @return Stream|null
+     */
     public function stream()
     {
         return $this->stream;
     }
 
+    /**
+     * The data that belongs to this event.
+     *
+     * Important: the payload object itself is not immutable. During design it was considered whether this should
+     * be immutable or not but at this stage I have left it mutable on purpose. This may however change so it is not
+     * recommended to rely on this while this notice is here.
+     *
+     * @return Event\Payload
+     */
     public function payload()
     {
         return $this->payload;
     }
 
+    /**
+     * Returns the date at which this event was emitted.
+     *
+     * @return \DateTimeInterface
+     */
     public function emittedAt()
     {
         return $this->emittedAt;
     }
 
+    /**
+     * Returns any meta-data that may be relevant for this event.
+     *
+     * Important: the metadata object itself is not immutable. During design it was considered whether this should
+     * be immutable or not but at this stage I have left it mutable on purpose. This may however change so it is not
+     * recommended to rely on this while this notice is here.
+     *
+     * @return Metadata
+     */
     public function metadata()
     {
         return $this->metadata;
     }
 
+    /**
+     * Returns the position of this event in the event stream.
+     *
+     * The event stream maintains the positions of all events in the stream so that a stream may be snapshotted and
+     * rebuilt later from that point onwards, and optimistic locking may be implemented.
+     *
+     * The position indicators range from 1 to infinity where 1 is the first event and each event thereafter should
+     * follow in time. The sequence should never be broken so if a number is missing in the sequence than that event
+     * stream is compromised and should be inspected to see if something is wrong.
+     *
+     * If this event is not yet emitted then it will not yet have a sequence number and this method will return null.
+     *
+     * @return int|null
+     */
     public function sequence()
     {
         return $this->sequence;
     }
 
+    /**
+     * Associates a clone of this event with the given stream and assigns it a new sequence number.
+     *
+     * @param Stream $stream
+     *
+     * @return Event
+     */
     public function withStream(Stream $stream)
     {
         $clone = clone $this;
@@ -78,6 +154,22 @@ final class Event
         return $clone;
     }
 
+    /**
+     * Creates a new event and populates it with the given data.
+     *
+     * @param string[] $data An array containing the following keys:
+     *
+     *     - id, the UUID for this new event
+     *     - payload, an array containing the data
+     *     - emitted_at, a string representation of the (UTC) date  and time at which the event was emitted, it is
+     *         recommended to use the MySQL DateTime format (Y-m-d H:i:s) to be certain that the right moment is
+     *         captured.
+     *     - metadata, an array containing metadata associated with the event
+     *     - sequence, the position in the stream of this event
+     *     - stream, a Stream object to which this event belongs
+     *
+     * @return static
+     */
     public static function fromArray(array $data)
     {
         $event = new static(
---
+++ tests/unit/Stream/EventTest.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace PhpInPractice\EventStore\Stream;
+
+use PhpInPractice\EventStore\Metadata;
+use PhpInPractice\EventStore\Stream;
+use Rhumsaa\Uuid\Uuid;
+
+/**
+ * @coversDefaultClass PhpInPractice\EventStore\Stream\Event
+ */
+class EventTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     * @covers ::__construct
+     * @covers ::id
+     */
+    public function it_has_an_identifier()
+    {
+        $id    = Event\id::generate();
+        $event = new Event($id, new Event\Payload());
+
+        $this->assertSame($id, $event->id());
+    }
+
+    /**
+     * @test
+     * @covers ::__construct
+     * @covers ::payload
+     */
+    public function it_has_a_payload()
+    {
+        $payload = new Event\Payload();
+        $event = new Event(Event\id::generate(), $payload);
+
+        $this->assertSame($payload, $event->payload());
+    }
+
+    /**
+     * @test
+     * @covers ::__construct
+     * @covers ::emittedAt
+     */
+    public function it_has_the_creation_date_and_time_by_default()
+    {
+        $event = new Event(Event\id::generate(), new Event\Payload());
+
+        $this->assertInstanceOf(\DateTimeImmutable::class, $event->emittedAt());
+
+        // since a current time is prone to race conditions we check if the generated timestamp falls between
+        // 2 seconds before now and 2 seconds after now; which is good enough
+        $this->assertGreaterThanOrEqual(time() - 2, $event->emittedAt()->getTimestamp());
+        $this->assertLessThanOrEqual(time() + 2, $event->emittedAt()->getTimestamp());
+    }
+
+    /**
+     * @test
+     * @covers ::__construct
+     * @covers ::emittedAt
+     */
+    public function creation_date_and_time_can_be_overridden()
+    {
+        $dateTime = new \DateTimeImmutable('2000-01-01 00:00');
+        $event = new Event(Event\id::generate(), new Event\Payload(), $dateTime);
+
+        $this->assertInstanceOf(\DateTimeImmutable::class, $event->emittedAt());
+        $this->assertSame($dateTime->getTimestamp(), $event->emittedAt()->getTimestamp());
+    }
+
+    /**
+     * @test
+     * @covers ::__construct
+     * @covers ::metadata
+     */
+    public function it_can_have_meta_data_with_it()
+    {
+        $metadata = new Metadata(['testKey' => 'testData']);
+        $event = new Event(Event\id::generate(), new Event\Payload(), null, $metadata);
+
+        $this->assertSame($metadata, $event->metadata());
+        $this->assertSame($metadata['testKey'], 'testData');
+    }
+
+    /**
+     * @test
+     * @covers ::__construct
+     * @covers ::sequence
+     * @covers ::stream
+     */
+    public function it_is_not_associated_with_a_stream_by_default_and_has_no_sequence()
+    {
+        $event = new Event(Event\id::generate(), new Event\Payload());
+        $this->assertNull($event->sequence());
+        $this->assertNull($event->stream());
+    }
+
+    /**
+     * @test
+     * @covers ::__construct
+     * @covers ::withStream
+     * @covers ::sequence
+     * @covers ::stream
+     */
+    public function associating_it_with_a_stream_creates_a_new_event_with_sequence_number()
+    {
+        $event = new Event(Event\Id::generate(), new Event\Payload());
+        $stream = new Stream(Id::generate());
+
+        // test if HEAD is tracked by incrementing HEAD a few times.
+        $stream->incrementHead();
+        $stream->incrementHead();
+
+        $this->assertAttributeSame(2, 'sequence', $stream);
+
+        $result = $event->withStream($stream);
+
+        $this->assertNotSame($event, $result);
+        $this->assertInstanceOf(Event::class, $result);
+        $this->assertNull($event->stream());
+        $this->assertSame($stream, $result->stream());
+        $this->assertSame(3, $result->sequence());
+        $this->assertSame(null, $event->sequence());
+        $this->assertAttributeSame(3, 'sequence', $stream);
+    }
+
+    /**
+     * @test
+     * @covers ::fromArray
+     */
+    public function it_can_be_reconstituted_from_array()
+    {
+        $uuid  = Uuid::uuid4();
+        $payload = ['payload'];
+        $stream = new Stream(Id::generate());
+
+        $event = Event::fromArray([
+            'id'         => $uuid,
+            'payload'    => $payload,
+            'emitted_at' => '2000-01-01 00:00',
+            'metadata'   => ['metadata'],
+            'sequence'   => 3,
+            'stream'     => $stream,
+        ]);
+
+        $this->assertEquals(Event\Id::fromString($uuid), $event->id());
+        $this->assertEquals(new Event\Payload($payload), $event->payload());
+        $this->assertEquals(new \DateTimeImmutable('2000-01-01 00:00'), $event->emittedAt());
+        $this->assertEquals(new Metadata(['metadata']), $event->metadata());
+        $this->assertEquals(new Metadata(['metadata']), $event->metadata());
+        $this->assertSame(3, $event->sequence());
+        $this->assertSame($stream, $event->stream());
+    }
+}
